0c8c9ae63e091a6b4a40cbdbe31bb5318ab72e49
Mischa Boender <mischa@boender.eu>
2018-07-30 21:43:33 +0200

    Added $PowerFIGletFontDirectory module variable

--- src/other/PreLoad.ps1
+++ src/other/PreLoad.ps1
@@ -1,8 +1,10 @@
 <#
  Put all code that must be run prior to function dot sourcing here.
 
- This is a good place for module variables as well. The only rule is that no 
- variable should rely upon any of the functions in your module as they 
+ This is a good place for module variables as well. The only rule is that no
+ variable should rely upon any of the functions in your module as they
  will not have been loaded yet. Also, this file cannot be completely
  empty. Even leaving this comment is good enough.
 #>
+
+$PowerFIGletFontDirectory = Join-Path -Path $PSScriptRoot -ChildPath 'fonts'
